1e8ab1e7aedf676ea9b318577350f38f4c20cfe2
FrankRM <frank.rodriguez@entelgy.com>
2018-03-05 22:04:48 -0500

    habilitndo CORS en el servidor

--- server/src/main/java/com/okta/developer/CRUDAngular5SpringBoot2/CoolCarController.java
+++ server/src/main/java/com/okta/developer/CRUDAngular5SpringBoot2/CoolCarController.java
@@ -1,5 +1,6 @@
 package com.okta.developer.CRUDAngular5SpringBoot2;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,6 +16,7 @@ class CoolCarController {
         this.repository = repository;
     }
 
+    @CrossOrigin(origins = "http://localhost:4200")
     @GetMapping("/cool-cars")
     public Collection<Car> coolCars() {
         return repository.findAll().stream()
