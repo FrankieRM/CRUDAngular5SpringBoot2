6359950f0406a2720cb99cf8bdd14915bd3a5a18
FrankRM <frank.rodriguez@entelgy.com>
2018-03-29 20:27:59 -0500

    Corrigiendo el tratado de CORS con FilterRegistrationBean

--- server/src/main/java/com/okta/developer/CRUDAngular5SpringBoot2/CrudAngular5SpringBoot2Application.java
+++ server/src/main/java/com/okta/developer/CRUDAngular5SpringBoot2/CrudAngular5SpringBoot2Application.java
@@ -3,9 +3,15 @@ package com.okta.developer.CRUDAngular5SpringBoot2;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.Ordered;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
+import java.util.Collections;
 import java.util.stream.Stream;
 
 @EnableResourceServer
@@ -28,5 +34,19 @@ public class CrudAngular5SpringBoot2Application {
                     });
             repository.findAll().forEach(System.out::println);
         };
+    }
+
+    @Bean
+    public FilterRegistrationBean simpleCorsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
+        config.setAllowedMethods(Collections.singletonList("*"));
+        config.setAllowedHeaders(Collections.singletonList("*"));
+        source.registerCorsConfiguration("/**", config);
+        FilterRegistrationBean bean = new FilterRegistrationBean<>(new CorsFilter(source));
+        bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        return bean;
     }
 }
